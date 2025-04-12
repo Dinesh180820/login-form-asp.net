8f644e1631a1b6fe02d3c23f1a140c2b4cd6ae77
Dinesh180820 <dineshseeni2k@gmail.com>
2025-04-12 17:25:43 +0530

    Delete Login-Form Project directory

--- Login-Form Project/SQLQuery1.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE UserDB;
-USE UserDB;
-CREATE TABLE Users( Id INT IDENTITY(1,1) PRIMARY KEY, FullName NVARCHAR(50), 
- Email NVARCHAR(50), Phone bigint,Username NVARCHAR(50) UNIQUE,NPassword NVARCHAR(50));
- SELECT * FROM Users;
- EXEC sp_rename 'Users.FName','FullName','COLUMN';
